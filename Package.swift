c2e9c55e88068159991537e61e639eb5e33ffecf
Renato Mendes <renatodz86@gmail.com>
2023-06-18 17:46:53 -0300

    Update ios version

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TSMarkdownParser",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9)
+        .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
